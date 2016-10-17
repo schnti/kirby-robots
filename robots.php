3afa5552f596dfe7793f97c822612415b580970c
Timo Schneider <tschneider@spirit21.de>
2016-10-17 17:58:02 +0200

    Fix robot folder paths.

--- robots.php
+++ robots.php
@@ -6,9 +6,9 @@ kirby()->routes(array(
         'action' => function () {
             return new Response('User-agent: *
 Disallow: /content/*.txt$
-Disallow: /kirby/
-Disallow: /site/
-Disallow: /panel/
+Disallow: /kirby
+Disallow: /site
+Disallow: /panel
 Disallow: /*.md$
 Sitemap: ' . u('sitemap.xml'), 'txt');
         }
